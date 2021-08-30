001ff8852dd893e6d3debd69be95a51f9ce4b310
Maxim <pyrozhog@gmail.com>
2021-08-30 09:57:50 +0300

    Remove redundant compile commands

--- generator/parsing/lexers/compile.sh
+++ generator/parsing/lexers/compile.sh
@@ -3,5 +3,3 @@
 PWD=`dirname $0`
 
 $PWD/../../RE-flex/bin/reflex $PWD/cpp_source.l −−outfile=$PWD/compiled/cpp_source.yy.cpp −−header-file=$PWD/compiled/cpp_source.yy.h
-$PWD/../../RE-flex/bin/reflex $PWD/json.l −−outfile=$PWD/compiled/json.yy.cpp −−header-file=$PWD/compiled/json.yy.h
-$PWD/../../RE-flex/bin/reflex $PWD/yaml.l −−outfile=$PWD/compiled/yaml.yy.cpp −−header-file=$PWD/compiled/yaml.yy.h
